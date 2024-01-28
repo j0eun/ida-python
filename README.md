ebd8465a63a251da5654b84b9130b14e6b8711ab
j0eun <kim950210@naver.com>
2024-01-28 10:50:16 +0900

    docs: update readme and comments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # IDA-Python
+
+바이너리 분석 시 IDA에서 사용할 수 있는 다양한 IDAPython 스크립트 템플릿
+
+## Templates
+
+- `decode_string` : 문자열 난독화 해제
--- decode_string/src/decode_string.py
+++ decode_string/src/decode_string.py
@@ -12,8 +12,8 @@ def get_str(ea):
 
     return s 
 
-# 난독화 해제 함수 슈도코드를 분석하여 Python 스크립트로 포팅
-# 난독화 해제 알고리즘은 바이너리마다 다르기에 그때그때 분석해서 decode 함수를 채워줘야 함
+# 난독화 해제 함수를 분석하여 Python 스크립트로 포팅
+# 난독화 해제 알고리즘은 바이너리마다 다르기에 그때그때 분석해서 decode 함수를 재작성해야 함
 def decode(ea_encoded):
     decoded = []
     encoded = get_str(ea_encoded)
@@ -24,26 +24,24 @@ def decode(ea_encoded):
         decoded.append(e ^ xorkey)
 
     for i in range(length):
-        ida_bytes.patch_byte(ea_encoded+i, decoded[i])         # 디코딩시킨 문자열로 패치
+        ida_bytes.patch_byte(ea_encoded+i, decoded[i])          # 디코딩시킨 문자열로 패치
 
-    ida_bytes.create_strlit(ea_encoded, length, idc.STRTYPE_C)    # idb에서 바이트 배열 문자열로 정의
+    ida_bytes.create_strlit(ea_encoded, length, idc.STRTYPE_C)  # idb에서 바이트 배열 문자열로 정의
 
-
-# 디코딩시킨 문자열을 가지고 또다시 디코딩 함수를 호출하는 일이 없도록 패치해준다.
-# opcode를 파싱할 때 쓰이는 오프셋이 바이너리마다 다르니 계산 필요
+# 디코딩시킨 문자열을 가지고 런타임에 또다시 디코딩 함수를 호출하는 일이 없도록 패치해준다.
 def main():
-    func_decode = idc.get_name_ea_simple('sub_1400010C0')            # 난독화 해제 함수
+    func_decode = idc.get_name_ea_simple('sub_1400010C0')       # 난독화 해제 함수
     xrefs = idautils.XrefsTo(func_decode, 0)
     for x in xrefs:
         if idc.print_insn_mnem(x.frm) != 'call':
             continue
 
-        var_name = idc.print_operand(x.frm - 0x11, 1)
+        var_name = idc.print_operand(x.frm - 0x11, 1)           # opcode를 파싱할 때 쓰이는 오프셋이 바이너리마다 다르니 계산 필요
         ea_encoded = idc.get_name_ea_simple(var_name)
         decode(ea_encoded)
 
         for ea in range(x.frm-10, x.frm+5):
-            ida_bytes.patch_byte(ea, 0x90)                     # 필요 없는 부분은 NOP으로 패치
+            ida_bytes.patch_byte(ea, 0x90)                      # 필요 없는 부분은 NOP으로 패치
 
 
 main()
