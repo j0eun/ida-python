60f28b57666759f4693ac950e2e31e124e0a191b
j0eun <kim950210@naver.com>
2024-01-28 10:18:44 +0900

    refactor: porting from old IDAPython APIs to newer APIs

--- decode_string/src/decode_string.py
+++ decode_string/src/decode_string.py
@@ -1,10 +1,13 @@
-from idc_bc695 import *
+import idc
+import idautils
+import ida_bytes
 
 def get_str(ea):
     s = []
     i = 0
-    while Byte(ea+i) != 0:
-        s.append(Byte(ea+i))
+
+    while ida_bytes.get_wide_byte(ea+i) != 0:
+        s.append(ida_bytes.get_wide_byte(ea+i))
         i += 1
 
     return s 
@@ -16,24 +19,31 @@ def decode(ea_encoded):
     encoded = get_str(ea_encoded)
     length = len(encoded)
     xorkey = 0xff
+
     for e in encoded:
         decoded.append(e ^ xorkey)
+
     for i in range(length):
-        PatchByte(ea_encoded+i, decoded[i])         # 디코딩시킨 문자열로 패치
-    create_strlit(ea_encoded, ea_encoded+length)    # idb에서 바이트 배열 문자열로 정의
+        ida_bytes.patch_byte(ea_encoded+i, decoded[i])         # 디코딩시킨 문자열로 패치
+
+    ida_bytes.create_strlit(ea_encoded, length, idc.STRTYPE_C)    # idb에서 바이트 배열 문자열로 정의
 
 
 # 디코딩시킨 문자열을 가지고 또다시 디코딩 함수를 호출하는 일이 없도록 패치해준다.
 # opcode를 파싱할 때 쓰이는 오프셋이 바이너리마다 다르니 계산 필요
-func_decode = LocByName('sub_1400010C0')            # 난독화 해제 함수
-gen_xrefs = XrefsTo(func_decode, 0)
-for x in gen_xrefs:
-    if GetMnem(x.frm) == 'call':
-        var_name = GetOpnd(x.frm - 0x11, 1)
-        ea_encoded = LocByName(var_name)
+def main():
+    func_decode = idc.get_name_ea_simple('sub_1400010C0')            # 난독화 해제 함수
+    xrefs = idautils.XrefsTo(func_decode, 0)
+    for x in xrefs:
+        if idc.print_insn_mnem(x.frm) != 'call':
+            continue
+
+        var_name = idc.print_operand(x.frm - 0x11, 1)
+        ea_encoded = idc.get_name_ea_simple(var_name)
         decode(ea_encoded)
+
         for ea in range(x.frm-10, x.frm+5):
-            PatchByte(ea, 0x90)                     # 필요 없는 부분은 NOP으로 패치
+            ida_bytes.patch_byte(ea, 0x90)                     # 필요 없는 부분은 NOP으로 패치
 
 
-        
+main()
